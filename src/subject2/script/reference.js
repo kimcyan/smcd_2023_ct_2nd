f4e4a3c92f5b40c39a90767eb6e643fcedcfecd4
kimcyan <116955327+kimcyan@users.noreply.github.com>
2023-10-03 10:47:50 +0900

    subject1 transition 추가

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,16 +1,9 @@
 const reference = [
   {
-    author: 'IS Creative Studio.',
-    title: 'Ficciones Typeface',
-    year: '2011',
-    sourceTitle: '',
-    link: 'https://www.behance.net/gallery/2656267/Ficciones-Typeface',
-  },
-  {
-    author: 'Jonathan Adler',
-    title: 'VERSAILLES PLAYING CARD SET',
+    author: '',
+    title: '',
     year: '',
     sourceTitle: '',
-    link: 'https://jonathanadler.com/products/versailles-playing-card-set?variant_id=31498434838562',
+    link: '',
   },
 ];
